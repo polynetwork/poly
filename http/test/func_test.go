4a83322dfb228deb042e5e27df4afb42a2e2ea2b
Allen <lvshuaino@gmail.com>
2020-02-03 14:29:01 +0800

    Remove ont and ong related testcase.

--- http/test/func_test.go
+++ http/test/func_test.go
@@ -20,14 +20,11 @@ package test
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/ontio/multi-chain/common"
 	"github.com/ontio/multi-chain/core/types"
 	"github.com/ontio/multi-chain/merkle"
-	"github.com/ontio/multi-chain/vm/neovm"
 	"github.com/ontio/ontology-crypto/keypair"
 	"github.com/stretchr/testify/assert"
-	"math/big"
 	"testing"
 )
 
@@ -75,7 +72,7 @@ func TestMerkleVerifier(t *testing.T) {
 }
 
 func TestTxDeserialize(t *testing.T) {
-	bys, _ := common.HexToBytes("00d1af758596f401000000000000204e000000000000b09ba6a4fe99eb2b2dc1d86a6d453423a6be03f02e0101011552c1126765744469736b506c61796572734c697374676a6f1082c6cec3a1bcbb5a3892cf770061e4b98200014241015d434467639fd8e7b4331d2f3fc0d4168e2d68a203593c6399f5746d2324217aeeb3db8ff31ba0fdb1b13aa6f4c3cd25f7b3d0d26c144bbd75e2963d0a443629232103fdcae8110c9a60d1fc47f8111a12c1941e1f3584b0b0028157736ed1eecd101eac")
+	bys, _ := common.HexToBytes("00d190ac06ff0000000000000000fddf0300000000000000000000000000000000000000000313496d706f72744f757465725472616e73666572fdb3030300000000000000ad000000fd7501fd720120366d36f82d953816ad92e1eddc83836d28ff6fa5089c07609137ad0e290875f208140000000000000014e553510e9f7eb980b3f225056d144529525cc5ec01000000000000000362746306756e6c6f636bfd1d01226d6a456f79794350734c7a4a3233784d58364d746931337a4d794e33366b7a6e3537ad42000000000000f15521023ac710e73e1410718530b2686ce47f12fa3c470a9eb6085976b70b01c64c9f732102c9dc4d8f419e325bbef0fe039ed6feaf2079a2ef7b27336ddb79be2ea6e334bf2102eac939f2f0873894d8bf0ef2f8bbdd32e4290cbf9632b59dee743529c0af9e802103378b4a3854c88cca8bfed2558e9875a144521df4a75ab37a206049ccef12be692103495a81957ce65e3359c114e6c2fe9f97568be491e3f24d6fa66cc542e360cd662102d43e29299971e802160a92cfcd4037e8ae83fb8f6af138684bebdc5686f3b9db21031e415c04cbc9b81fbee6e04d8c902e8f61109a2c9883a959ba528c52698c055a57ae142186fe74983e0016359c7c1b9063448fc8813b8700fd160200ad00000011cbe13241a6a8f9711d511222d9ba69943aa32ea2cf057945529ff0014bed3c0540211ce0c33a78753097e8f9bc59770ed175c41e7ec6b8cdda325bc18c593306dc77ea250699f3c11a819c6a04872cc29178d5d79d9d65834ab084988d6f48e7e04029f7776bcd13dadce2153bf487f72f7dc216fcb12648e29f1e868feb96c4823f5fcd9ce8c16d332fbab15fc4fbed337514ed0be9bc1a79aede0ff5aa7fcd621440d3afda11df726ce1113357ce78d50a3ef58c1fdf398beee5c13d934fa4ad5af9926ea3cb72844a84c9ac28824be66b84a9a2a300d6bb38893bdd13d95c519d87405265f6e83dc0b6a89d426ed2f6d0acd71b90e042d2deb3658965620e0af25c5ce22b192b7e74bd2249f80fe1388ee613b1d02ff0b5582beaffa719fe9765372440bae0e1f3103f0fed5716dbae10fe4bffe6688992820d504595521ad82d302c5842efd4ce7024922c7aa4f006ff83fc212e10f4cf909852a41ebb727c96e849830521023967bba3060bf8ade06d9bad45d02853f6c623e4d4f52d767eb56df4d364a99f210215865baab70607f4a2413a7a9ba95ab2c3c0202d5b7731c6824eef48e899fc9021035eb654bad6c6409894b9b42289a43614874c7984bde6b03aaf6fc1d0486d9d45210253ccfd439b29eca0fe90ca7c6eaa1f98572a054aa2d1d56e72ad96c466107a852103f1095289e7fddb882f1cb3e158acc1c30d9de606af21c97ba851821e8b6ea5350001010042051c33e385b99bac45dbf50e9e2074f399f38170cd570ddf7be125bbc908d5bf496e7ff97ace139b2774332e858a194af94249e0b23a8bd212f43247c3b1b6a9cc3f010023120503d101838807ec4079a46fe98d6bd9a0690abcbd8ce16e0fbc4520c7c7ef7885db0100")
 	_, err := types.TransactionFromRawBytes(bys)
 	assert.Nil(t, err)
 }
@@ -95,44 +92,4 @@ func TestMultiPubKeysAddress(t *testing.T) {
 
 	_, err = types.AddressFromMultiPubKeys([]keypair.PublicKey{pk, pk2}, 1)
 	assert.Nil(t, err)
-}
-
-func BuildSmartContractParamInter(builder *neovm.ParamsBuilder, smartContractParams []interface{}) error {
-	for i := len(smartContractParams) - 1; i >= 0; i-- {
-		switch v := smartContractParams[i].(type) {
-		case bool:
-			builder.EmitPushBool(v)
-		case int:
-			builder.EmitPushInteger(big.NewInt(int64(v)))
-		case uint:
-			builder.EmitPushInteger(big.NewInt(int64(v)))
-		case int32:
-			builder.EmitPushInteger(big.NewInt(int64(v)))
-		case uint32:
-			builder.EmitPushInteger(big.NewInt(int64(v)))
-		case int64:
-			builder.EmitPushInteger(big.NewInt(int64(v)))
-		case common.Fixed64:
-			builder.EmitPushInteger(big.NewInt(int64(v.GetData())))
-		case uint64:
-			val := big.NewInt(0)
-			builder.EmitPushInteger(val.SetUint64(uint64(v)))
-		case string:
-			builder.EmitPushByteArray([]byte(v))
-		case *big.Int:
-			builder.EmitPushInteger(v)
-		case []byte:
-			builder.EmitPushByteArray(v)
-		case []interface{}:
-			err := BuildSmartContractParamInter(builder, v)
-			if err != nil {
-				return err
-			}
-			builder.EmitPushInteger(big.NewInt(int64(len(v))))
-			builder.Emit(neovm.PACK)
-		default:
-			return fmt.Errorf("unsupported param:%s", v)
-		}
-	}
-	return nil
 }
