35af3ea3bb71a6be85f0f008d6490a3e51d872d3
Allen <lvshuaino@gmail.com>
2020-02-06 10:36:32 +0800

    Add relayer manager testcase.

--- native/service/governance/relayer_manager/param_test.go
+++ native/service/governance/relayer_manager/param_test.go
@@ -1,11 +1,19 @@
 package relayer_manager
 
-import "testing"
+import (
+	"github.com/ontio/multi-chain/common"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestRelayerListParam_Serialization(t *testing.T) {
+	params := new(RelayerListParam)
+	params.AddressList = [][]byte{{1, 2, 4, 6}, {1, 4, 5, 7}, {1, 3, 5, 7, 9}}
+	sink := common.NewZeroCopySink(nil)
+	params.Serialization(sink)
 
-}
-
-func TestRelayerListParam_Deserialization(t *testing.T) {
-
+	source := common.NewZeroCopySource(sink.Bytes())
+	var p RelayerListParam
+	err := p.Deserialization(source)
+	assert.Nil(t, err)
 }
