f39f522561b9a02412ac07ba4032d385390a3f68
tanyuan <1067598718@qq.com>
2020-11-26 14:38:46 +0800

    remove unused code

--- cmd/sigsvr/handler.go
+++ cmd/sigsvr/handler.go
@@ -24,10 +24,4 @@ func init() {
 	DefCliRpcSvr.RegHandler("createaccount", handlers.CreateAccount)
 	DefCliRpcSvr.RegHandler("exportaccount", handlers.ExportAccount)
 	DefCliRpcSvr.RegHandler("sigdata", handlers.SigData)
-	DefCliRpcSvr.RegHandler("sigrawtx", handlers.SigRawTransaction)
-	DefCliRpcSvr.RegHandler("sigmutilrawtx", handlers.SigMutilRawTransaction)
-	DefCliRpcSvr.RegHandler("sigtransfertx", handlers.SigTransferTransaction)
-	DefCliRpcSvr.RegHandler("signeovminvoketx", handlers.SigNeoVMInvokeTx)
-	DefCliRpcSvr.RegHandler("signeovminvokeabitx", handlers.SigNeoVMInvokeAbiTx)
-	DefCliRpcSvr.RegHandler("signativeinvoketx", handlers.SigNativeInvokeTx)
 }
