bcb7235cda1a34127cdf9fccfb2f1c305ccda9bb
zhuoqian <zhuoqian10@gmail.com>
2021-05-26 16:55:26 +0800

    fix N3 header verification bug

--- native/service/header_sync/neo3/header_sync.go
+++ native/service/header_sync/neo3/header_sync.go
@@ -19,8 +19,10 @@ package neo3
 
 import (
 	"fmt"
+	"github.com/joeqian10/neo3-gogogo/helper"
 	"github.com/polynetwork/poly/native"
 	"github.com/polynetwork/poly/native/service/governance/node_manager"
+	"github.com/polynetwork/poly/native/service/governance/side_chain_manager"
 	hscommon "github.com/polynetwork/poly/native/service/header_sync/common"
 
 	"github.com/polynetwork/poly/common"
@@ -50,7 +52,6 @@ func (this *Neo3Handler) SyncGenesisHeader(native *native.NativeService) error {
 		return fmt.Errorf("Neo3Handler SyncGenesisHeader, checkWitness error: %v", err)
 	}
 	// Deserialize neo block header
-
 	header := new(NeoBlockHeader)
 	if err := header.Deserialization(common.NewZeroCopySource(params.GenesisHeader)); err != nil {
 		return fmt.Errorf("Neo3Handler SyncGenesisHeader, deserialize header err: %v", err)
@@ -78,6 +79,10 @@ func (this *Neo3Handler) SyncBlockHeader(native *native.NativeService) error {
 	if err != nil {
 		return fmt.Errorf("Neo3Handler SyncBlockHeader, the consensus validator has not been initialized, chainId: %d", params.ChainID)
 	}
+	sideChain, err := side_chain_manager.GetSideChain(native, params.ChainID)
+	if err != nil {
+		return fmt.Errorf("neo3 MakeDepositProposal, side_chain_manager.GetSideChain error: %v", err)
+	}
 	var newNeoConsensus *NeoConsensus
 	for _, v := range params.Headers {
 		header := new(NeoBlockHeader)
@@ -85,7 +90,7 @@ func (this *Neo3Handler) SyncBlockHeader(native *native.NativeService) error {
 			return fmt.Errorf("Neo3Handler SyncBlockHeader, NeoBlockHeaderFromBytes error: %v", err)
 		}
 		if !header.GetNextConsensus().Equals(neoConsensus.NextConsensus) && header.GetIndex() > neoConsensus.Height {
-			if err = verifyHeader(native, params.ChainID, header); err != nil {
+			if err = verifyHeader(native, params.ChainID, header, helper.BytesToUInt32(sideChain.ExtraInfo)); err != nil {
 				return fmt.Errorf("Neo3Handler SyncBlockHeader, verifyHeader error: %v", err)
 			}
 			newNeoConsensus = &NeoConsensus{
