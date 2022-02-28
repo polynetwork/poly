1cd7db67a1c89b29b4f6bbaa066cb0474b0c9f97
rain <206131521@qq.com>
2022-02-28 19:10:18 +0800

    add HSC header_sync 0.1

--- native/service/header_sync/hsc/header_sync_test.go
+++ native/service/header_sync/hsc/header_sync_test.go
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with The poly network .  If not, see <http://www.gnu.org/licenses/>.
  */
-package heco
+package hsc
 
 import (
 	"bytes"
